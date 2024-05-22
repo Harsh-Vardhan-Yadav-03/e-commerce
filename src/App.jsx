02f84e0c5de1c8546d84335a99ee007436528691
Harsh Vardhan Yadav <165200197+Harsh-Vardhan-Yadav-03@users.noreply.github.com>
2024-05-22 16:28:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import Home from './Pages/Home'
-import { Carousel } from "./components/Carousel";
-import { slides } from"./data/carouselData.json"; 
+
 
 function App() {
 
@@ -9,7 +8,7 @@ function App() {
   return (
    <>
    <Home/>
-   <Carousel data= { slides }/>
+   
    </>
   );
 }
